bc66cab5716c59c956e0068f25a5fca6a04c4bdb
Juan Arredondo <juan.arredondo@gmail.com>
2021-05-18 17:40:00 -0400

    Updated quotes_spider, created author_spider, and stored data as json files

--- scraping_2/scraping_2/spiders/quotes_spider.py
+++ scraping_2/scraping_2/spiders/quotes_spider.py
@@ -2,21 +2,23 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     # name: identifies the Spider. It must be unique within a project, that is, you can’t set the same name for different Spiders.
-    name = "quotes"
+    name = 'quotes'
 
-    # start_requests(): must return an iterable of Requests (you can return a list of requests or write a generator function) which the Spider will begin to crawl from. Subsequent requests will be generated successively from these initial requests.
     def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        url = 'http://quotes.toscrape.com/'
+        tag = getattr(self, 'tag', None)
+        if tag is not None:
+            url = url + 'tag/' + tag
+        yield scrapy.Request(url, self.parse)
 
-    # parse(): a method that will be called to handle the response downloaded for each of the requests made. The response parameter is an instance of TextResponse that holds the page content and has further helpful methods to handle it.
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
