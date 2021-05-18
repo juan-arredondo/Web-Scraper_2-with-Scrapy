295fde59bc0c51dd08e9877e81d59510a17366ef
Juan Arredondo <juan.arredondo@gmail.com>
2021-05-18 10:20:11 -0400

    creation of spider

---
+++ scraping_2/scraping_2/spiders/quotes_spider.py
@@ -0,0 +1,22 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    # name: identifies the Spider. It must be unique within a project, that is, you can’t set the same name for different Spiders.
+    name = "quotes"
+
+    # start_requests(): must return an iterable of Requests (you can return a list of requests or write a generator function) which the Spider will begin to crawl from. Subsequent requests will be generated successively from these initial requests.
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    # parse(): a method that will be called to handle the response downloaded for each of the requests made. The response parameter is an instance of TextResponse that holds the page content and has further helpful methods to handle it.
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f'quotes-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
